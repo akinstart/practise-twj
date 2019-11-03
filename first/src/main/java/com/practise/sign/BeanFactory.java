2b108f2a09ccd33f663444fb288ac1ac473ce0dc
陶文俊 <taowenjun@bizvane.cn>
2019-11-03 08:25:17 +0800

    branch分支开发

---
+++ first/src/main/java/com/practise/sign/BeanFactory.java
@@ -0,0 +1,12 @@
+package com.practise.sign;
+
+/**
+ * @author taowj
+ * @date 2019/11/1
+ */
+public class BeanFactory {
+
+	public static void main(String[] args) {
+		System.out.println("ceshi ");
+	}
+}
