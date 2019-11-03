dada2bded5e5dba347790ab2e8848f6b6ec9b5bb
陶文俊 <taowenjun@bizvane.cn>
2019-11-03 08:13:24 +0800

    第一步

---
+++ first/src/main/java/Application.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @author taowj
+ * @date 2019/11/1
+ */
+@SpringCloudApplication
+public class Application{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+}
