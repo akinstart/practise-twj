91701753148062890d438fde495d8ee2d364be4a
陶文俊 <taowenjun@bizvane.cn>
2019-11-03 08:37:56 +0800

    主分支测试

---
+++ first/src/main/java/com/practise/sign/Singleton.java
@@ -0,0 +1,12 @@
+package com.practise.sign;
+
+/**
+ * @author taowj
+ * @date 2019/11/1
+ */
+public class Singleton {
+
+	public static void main(String[] args) {
+		System.out.println("主分支测试");
+	}
+}
