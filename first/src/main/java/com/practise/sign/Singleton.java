d1756ef6aad41ac9b992cc044b3ed492d674b0a6
陶文俊 <taowenjun@bizvane.cn>
2019-11-03 08:44:47 +0800

    本地合并分支代码提交

--- first/src/main/java/com/practise/sign/Singleton.java
+++ first/src/main/java/com/practise/sign/Singleton.java
@@ -1,8 +1,12 @@
 package com.practise.sign;
 
+/**
+ * @author taowj
+ * @date 2019/11/1
+ */
 public class Singleton {
 
 	public static void main(String[] args) {
-		System.out.println("还是测试");
+		System.out.println("最终结果测试");
 	}
 }
